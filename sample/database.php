27e91565e10f3615fcabe46d682e4743bf798f77
tomo898 <105865731+tomo898@users.noreply.github.com>
2022-05-20 02:16:10 +0900

    Delete sample directory

--- sample/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('DB_HOST',   'localhost');//データベースのホスト名又はIPアドレス
-define('DB_USER',   'root');//MySQLのユーザ名
-define('DB_PASSWD', 'root');//MySQLのパスワード
-define('DB_NAME',   'anime');//データベース名
